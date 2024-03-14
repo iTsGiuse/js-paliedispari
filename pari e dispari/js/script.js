bf2f2ea342e34684aab3bfebfbc003a7473bc490
iTsGiuse <giuse.amato@icloud.com>
2024-03-14 18:50:19 +0100

    feat: add "verifica per l'inserimento del numero da parte dell'utente. Il numero effettivamente è compreso da 1 a 5".

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,44 +1,63 @@
+/* DICHIARAIONE VARIABILI */
+let tipoNumero;
+let numero = 0;
+let i = 0;
+let sum = 0;
+let risultato;
+
 /* L’utente sceglie pari o dispari */
-let tipoNumero = prompt('Pari o dispari?');
+
+tipoNumero = prompt ('Pari o dispari?');
+
+
+/* while (i < 1) {
+    if (tipoNumero = 'pari') {
+        tipoNumero = prompt('Pari o dispari?');
+    } else if (tipoNumero = 'dispari') {
+        tipoNumero = prompt('Pari o dispari?');
+    } else {
+        i=2;
+    }
+} */
+
 
 /* Utente inserisce un numero da 1 a 5. */
 
-let numero = parseInt(prompt('Inserisci un numero tra 1 e 5'));
-console.log(numero);
-
-
-// let numero = 0;
-//let i = 0;
-
-//while (i = 1) {
+while (i < 1) {   /* DEVO CAMBIARE LA VARIABILE i? */
     
-//    numero = parseInt(prompt('Inserisci un numero tra 1 e 5'));
-//    console.log(numero);
+   numero = parseInt(prompt('Inserisci un numero tra 1 e 5'));
+   console.log('Il numero inserito è ' + numero);
 
-//    if (numero => 1 && numero <= 5){
-//        i++;
-//    } else if (numero < 1 && numero > 6) {
-//        numero = parseInt(prompt('Inserisci un numero tra 1 e 5'));
-//    }
-
-//} 
-
-/* Generiamo un numero random (sempre da 1 a 5) 
-per il computer (usando una funzione) */
-function numeroGenerato () {
-    
-    const numeroRandom = Math.floor(Math.random() * 6);
-    console.log(numeroRandom);
-    return numeroRandom;
+   if (numero > 0 && numero < 6){
+       i=2;
+   } else if (numero < 1 && numero > 5) {
+       numero = parseInt(prompt('Inserisci un numero tra 1 e 5'));
+   }
 
 }
 
 /* Sommiamo i due numeri */
-let sum = numeroGenerato () + numero;
-console.log (sum);
+sum = numeroGenerato () + numero;
+console.log('Il numero inserito è ' + sum);
 
 /* Stabiliamo se la somma dei due numeri è pari o dispari  */
-let risultato;
+
+
+pariDispari();
+
+/* Dichiariamo chi ha vinto */
+alert('il vincitore ' + risultato + ' ' + sum);
+
+
+/* FUNZIONI */
+
+function numeroGenerato () {
+    
+    const numeroRandom = Math.floor(Math.random() * 6);
+    console.log('Il numero generato è ' +numeroRandom);
+    return numeroRandom;
+
+}
 
 function pariDispari(){
     
@@ -47,8 +66,3 @@ function pariDispari(){
     } else if (sum % 2 !== 0)
         risultato = 'è dispari';
 }
-
-pariDispari();
-
-/* Dichiariamo chi ha vinto */
-alert('il vincitore ' + risultato + ' ' + sum);
